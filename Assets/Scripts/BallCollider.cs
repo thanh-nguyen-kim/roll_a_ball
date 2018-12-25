e3411cf72ea51c9daaa95f618d80b0a1c1db6793
KamperTee <thanhnguyenkim12@gmail.com>
2018-12-25 11:24:45 +0700

    Initial roll_a_ball project

---
+++ Assets/Scripts/BallCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollider : MonoBehaviour
+{
+    [SerializeField] private GameObject endGamePanel;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            FindObjectOfType<BallController>().isAlive = false;
+            endGamePanel.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//this script controll the movement of the ball base on keyboard input
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private Transform ballCollider;
+    [SerializeField] private float speed, laneSpeed;
+    private Vector3 laneTargetPos = Vector3.zero;
+    public bool isAlive = false;
+    private void Start()
+    {
+        isAlive = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAlive)
+        {
+            float moveVal = Input.GetAxis("Horizontal");
+            if (moveVal != 0)
+                laneTargetPos = new Vector3(Mathf.Clamp(laneTargetPos.x + moveVal / 5, -0.75f, 0.75f), 0, 0);
+            else
+                laneTargetPos = ballCollider.localPosition;
+            transform.Translate(transform.forward * Time.deltaTime * speed);
+            ballCollider.localPosition = Vector3.MoveTowards(ballCollider.localPosition, laneTargetPos, Time.deltaTime * laneSpeed);
+            ballCollider.Rotate(new Vector3(10, 0, 0));
+        }
+    }
+}
